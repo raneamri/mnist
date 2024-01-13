f49f454f00fecb64598c69084425a9c9309085d1
imraneamri <mohammad.amri@ucdconnect.ie>
2024-01-13 15:28:34 +0000

    __main__ patch & README.md updated

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# MNIST NN
+# MNIST
+![PyPI - Version](https://img.shields.io/pypi/v/numpy)
+![Static Badge](https://img.shields.io/badge/dataset-mnist-purple)
+![Static Badge](https://img.shields.io/badge/neural%20network-vanilla-yellow)
+
+This project is a 2 layer neural network that learns to recognise handwritten numbers by being fed the famous [MNIST dataset](https://huggingface.co/datasets/mnist) as a .csv. This project uses no tensorflow, pytorch etc., solely math.
+
+---------------------------------------
+* [Settings](#settings)
+* [Forward Propagation](#forward-propagation)
+* [Backward Propagation](#backward-propagation)
+* [Update Parameters](#update-parameters)
+
+---------------------------------------
+
+## Settings
+The script includes a set of tweakable constants:
+* `Iterations`: How many iterations the neural network will loop.
+* `DISPLAY_REG`: How often the script will display the neural network's current accuracy.
+* `IMG_SIZE`: The number of pixels in the input image.
+* `DATASET_PARTITION`: Where the data should be parted for crossvalidation.
+* `TEST_PREDICTIONS`: How many times the neural network will be tried for predictions upon completing training.
+* `DATASET_FILE`: Input path for the data.
+
+## Forward Propagation
+During forward propagation, the neural network takes images and learns to create predictions out of them:
+* A<sup>0</sup>: This is the input layer (layer 0) of the neural network. It simply receives the `IMAGE_SIZE` number of pixels into each node.
+
+* Z<sup>1</sup>: Unactivated first layer. Z<sup>1</sup> is obtained by applying a weight obtained from the connections between nodes in the prior layer (W<sup>1</sup>) and a bias (b<sup>1</sup>) to the input layer (A<sup>0</sup>). Or, Z<sup>1</sup> = W<sup>1</sup> * A<sup>0</sup> + b<sup>1</sup>.
+* A<sup>1</sup>: First layer. A<sup>1</sup> is obtained by putting Z<sup>1</sup> through an activation function. The activation function I use is Exponential Linear Unit, or ELU.
+
+* Z<sup>2</sup>: Unactivated second layer. Z<sup>2</sup> is obtained by applying a weight obtained from the connections between nodes in the prior layer (W<sup>2</sup>) and a bias (b<sup>2</sup>) to the prior layer (A<sup>1</sup>). Or, Z<sup>2</sup> = W<sup>2</sup> * A<sup>1</sup> + b<sup>2</sup>.
+* A<sup>2</sup>: Second and final layer. A<sup>2</sup> is obtained from passing Z<sup>2</sup> through an activation function. This time we're using softmax, which will assign a probability to each node in this output layer.
+
+## Backward Propagation
+Backward propagation is a method of improving the algorithm as it learns. This is done by taking the prediction, measuring how much it deviated from the image's label and working backwards.
+* dZ<sup>2</sup>: A measure of the error in the second layer. It's obtained by taking the predictions and subtracting the labels from them. For that, we one-hot encode the label as Y. dZ<sup>2</sup> = A<sup>2</sup> - Y
+* dW<sup>2</sup>: The derivative of the loss function with respect to the weights in layer 2. dW<sup>2</sup> = 1/m * dZ<sup>2</sup> * A<sup>1</sup>.T. (Where .T is transposition of a matrix or vector)
+* db<sup>2</sup>: This is the average of the absolute error. db<sup>2</sup> = 1/m * Σ dZ<sup>2</sup>.
+
+* dZ<sup>1</sup>: A measure of the error in the first layer. This formula essentially performs forward propagation in reverse. dZ<sup>1</sup> = W<sup>2</sup>.T * dZ<sup>1</sup> * g'() where g'() is the derivative of the activation function.
+* dW<sup>1</sup>: The derivative of the loss function with respect to the weights in layer 1. dW<sup>1</sup> = 1/m * dZ<sup>1</sup> * X.T.
+* db<sup>1</sup>: db<sup>1</sup> = 1/m * Σ dZ<sup>1</sup>.
+
+## Update Parameters
+After successful forward & backward propagation, the algorithm updates a hyperparameter α in this fashion:
+* W<sup>1</sup> := W<sup>1</sup> - αdW<sup>1</sup>
+* b<sup>1</sup> := b<sup>1</sup> - αdb<sup>1</sup>
+* W<sup>2</sup> := W<sup>2</sup> - αdW<sup>2</sup>
+* b<sup>2</sup> := b<sup>2</sup> - αdb<sup>2</sup>
+
+α, being a hyperparameter, isn't set by gradient descent, but by the end-user. α can be interpreted as the learning rate.
+
+After that, the algorithm loops back to forward propagation.
+
+![Example](https://github.com/raneamri/mnist/img/example.png)
--- mnist_nn.py
+++ mnist_nn.py
@@ -2,39 +2,54 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 
-# constants
-ITERATIONS = 300
-DISPLAY_REG = 50
+ITERATIONS = 500
+DISPLAY_REG = 10
 IMG_SIZE = 784
 DATASET_PARTITION = 1_000
-TEST_PREDICTIONS = 100
-DATASET_FILE = "mnist_sample.csv"
+TEST_PREDICTIONS = 8
+DATASET_FILE = "sample.csv"
 
-# read data in
-data = pd.read_csv(DATASET_FILE)
-# prepare data, collect its dimensions & shuffle
-data = np.array(data)
-rows, cols = data.shape
-np.random.shuffle(data)
+def main():
+    # read data in
+    data = pd.read_csv(DATASET_FILE)
+    # prepare data, collect its dimensions & shuffle
+    data = np.array(data)
+    rows, cols = data.shape
+    np.random.shuffle(data)
 
-# prepare cross-validation data
-# note: we transpose all matrices that we feed the model
-#       this allows us to separate the labels and images easier by simply
-#       extracting the first row
-data_crossv = data[0:1000].T
-y_crossv = data_crossv[0]
-x_crossv = data_crossv[1:cols]
-x_crossv = x_crossv / 255.
+    # prepare cross-validation data
+    # note: we transpose all matrices that we feed the model
+    #       this allows us to separate the labels and images easier by simply
+    #       extracting the first row
+    data_crossv = data[0:DATASET_PARTITION].T
+    y_crossv = data_crossv[0]
+    x_crossv = data_crossv[1:cols]
+    x_crossv = x_crossv / 255.0
 
-data_train = data[1000:rows].T
-y_train = data_train[0]
-x_train = data_train[1:cols]
-x_train = x_train / 255.
-_, m_train = x_train.shape
+    data_train = data[DATASET_PARTITION:rows].T
+    y_train = data_train[0]
+    x_train = data_train[1:cols]
+    x_train = x_train / 255.0
+    _, m_train = x_train.shape
+
+    W1, b1, W2, b2 = gradient_descent(x_train, y_train, 0.10, ITERATIONS, rows)
+
+    for x in range(TEST_PREDICTIONS):
+        test_prediction(x, W1, b1, W2, b2, x_train, y_train)
+
+    dev_predictions = make_predictions(x_crossv, W1, b1, W2, b2)
+    print(
+        "Deployed accuracy: ",
+        round(get_accuracy(dev_predictions, y_crossv) * 100, 3),
+        "%\n",
+    )
+
+    return
+
 
 # starting parameters for the neural network
 def init_params():
-    W1 = np.random.rand(10, 784) - 0.5
+    W1 = np.random.rand(10, IMG_SIZE) - 0.5
     b1 = np.random.rand(10, 1) - 0.5
     W2 = np.random.rand(10, 10) - 0.5
     b2 = np.random.rand(10, 1) - 0.5
@@ -44,18 +59,22 @@ def init_params():
 def ELU(Z, a=1.0):
     return np.where(Z <= 0, a * (np.exp(Z) - 1), Z)
 
+
 def ELU_deriv(Z, a=1.0):
     return np.where(Z <= 0, a * np.exp(Z), 1)
+
 
 def softmax(Z):
     prob = np.exp(Z) / sum(np.exp(Z))
     return prob
+
 
 def one_hot(Y):
     one_hot_Y = np.zeros((Y.size, Y.max() + 1))
     one_hot_Y[np.arange(Y.size), Y] = 1
     one_hot_Y = one_hot_Y.T
     return one_hot_Y
+
 
 def forward_prop(W1, b1, W2, b2, X):
     Z1 = W1.dot(X) + b1
@@ -64,7 +83,8 @@ def forward_prop(W1, b1, W2, b2, X):
     A2 = softmax(Z2)
     return Z1, A1, Z2, A2
 
-def backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
+
+def backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y, rows):
     one_hot_Y = one_hot(Y)
     dZ2 = A2 - one_hot_Y
     dW2 = 1 / rows * dZ2.dot(A1.T)
@@ -74,26 +94,30 @@ def backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
     db1 = 1 / rows * np.sum(dZ1)
     return dW1, db1, dW2, db2
 
+
 def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
     W1 = W1 - alpha * dW1
-    b1 = b1 - alpha * db1    
-    W2 = W2 - alpha * dW2  
-    b2 = b2 - alpha * db2    
+    b1 = b1 - alpha * db1
+    W2 = W2 - alpha * dW2
+    b2 = b2 - alpha * db2
     return W1, b1, W2, b2
+
 
 def get_predictions(A2):
     return np.argmax(A2, 0)
+
 
 def get_accuracy(predictions, Y):
     print(predictions, Y)
 
     return np.sum(predictions == Y) / Y.size
 
-def gradient_descent(X, Y, alpha, iterations):
+
+def gradient_descent(X, Y, alpha, iterations, rows):
     W1, b1, W2, b2 = init_params()
     for i in range(iterations):
         Z1, A1, Z2, A2 = forward_prop(W1, b1, W2, b2, X)
-        dW1, db1, dW2, db2 = backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y)
+        dW1, db1, dW2, db2 = backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y, rows)
         W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
         if i % DISPLAY_REG == 0:
             print("\nIteration: ", i)
@@ -101,27 +125,25 @@ def gradient_descent(X, Y, alpha, iterations):
             print(get_accuracy(predictions, Y))
     return W1, b1, W2, b2
 
-W1, b1, W2, b2 = gradient_descent(x_train, y_train, 0.10, ITERATIONS)
 
 def make_predictions(X, W1, b1, W2, b2):
     _, _, _, A2 = forward_prop(W1, b1, W2, b2, X)
     predictions = get_predictions(A2)
     return predictions
 
-def test_prediction(index, W1, b1, W2, b2):
+
+def test_prediction(index, W1, b1, W2, b2, x_train, y_train):
     current_image = x_train[:, index, None]
     prediction = make_predictions(x_train[:, index, None], W1, b1, W2, b2)
     label = y_train[index]
     print("Prediction: ", prediction)
     print("Label: ", label)
-    
+
     current_image = current_image.reshape((28, 28)) * 255
     plt.gray()
-    plt.imshow(current_image, interpolation='nearest')
+    plt.imshow(current_image, interpolation="nearest")
     plt.show()
 
-for x in range(TEST_PREDICTIONS):
-    test_prediction(x, W1, b1, W2, b2)
 
-dev_predictions = make_predictions(x_crossv, W1, b1, W2, b2)
-print("Deployed accuracy: ", round(get_accuracy(dev_predictions, y_crossv) * 100, 3), "%\n")
+if __name__ == "__main__":
+    main()
